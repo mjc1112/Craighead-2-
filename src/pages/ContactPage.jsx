b9f1a8c6a6d43dd81f72d2c53076b4049db93ad6
mjc1112 <mjc111264@gmail.com>
2025-12-05 11:51:30 +0000

    Add ContactPage component with basic structure

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContactPage() {
+  return (
+    <section id="contact" className="cb-section cb-section--contact">
+      {/* Contact content goes here */}
+    </section>
+  );
+}
+
+export default ContactPage;
